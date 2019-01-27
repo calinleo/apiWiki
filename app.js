75a946136ca85e3c630fc7a9c947e5ffd82f77fc
Calin Leordean <calinleordean@Calins-MacBook-Pro.local>
2019-01-27 12:39:35 -0500

    Connected to External API - https://randomuser.me/api - console.log

--- app.js
+++ app.js
@@ -3,5 +3,20 @@ window.onload = function(){
 
     function showResults() {
         console.log('event listener works')
+        var url = 'https://randomuser.me/api';
+        ajaxJs(url, function(response){
+            console.log(response)
+        })
     }
+
+    function ajaxJs (url, callback){
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function(){
+            console.log(xhr)
+            callback('readychange works')
+        }
+        xhr.open('GET', url, true) // true for Asynchronous
+        xhr.send();
+    }
+
 }
